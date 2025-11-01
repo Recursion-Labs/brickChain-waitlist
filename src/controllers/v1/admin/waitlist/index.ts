7252745c962de55a828d22b09c9f1e0205e9d805
samarth3301 <samarplayz1337@gmail.com>
2025-11-01 19:57:22 +0530

    feat: add resend service for sending OTP emails and update package dependencies

---
+++ src/controllers/v1/admin/waitlist/index.ts
@@ -0,0 +1,1 @@
+export { default as waitlistController } from "./waitlist.controller"
---
+++ src/services/resend.service.ts
@@ -0,0 +1,24 @@
+import { Resend } from "resend";
+import fs from "fs";
+import path from "path";
+import { logger } from "@/config/logger";
+
+const resendClient = new Resend(process.env.RESEND_API_KEY!);
+
+export async function sendOTP(email: string) {
+	const templatePath = path.join(__dirname, "../templates/applicationReceived.html");
+	let html = "";
+	try {
+		const template = fs.readFileSync(templatePath, "utf8");
+		html = template;
+	} catch (err) {
+		logger.warn("Failed to load template, falling back to simple OTP HTML:", err);
+	}
+
+	await resendClient.emails.send({
+		from: "otps@techsolace.tech",
+		to: email,
+		subject: "Your OTP Code",
+		html,
+	});
+}
